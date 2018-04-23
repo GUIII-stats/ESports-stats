beb5e91a8fa65fe68b1fc0edb9009a9aa643daea
Sean Mccrobie <seanmccrobie@Seans-MacBook-Air.local>
2018-04-23 14:44:08 -0400

    added back in style

--- playercard.php
+++ playercard.php
@@ -95,6 +95,166 @@ print "<h3> Jun TY Tae Yang - Team Splyce </h3>" ;
 </div>
 
 
-
 </body>
 </html>
+
+
+
+<style>
+h3{
+  font-family: starfont;
+  text-decoration: underline;
+  margin-bottom: 0px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  color: black;
+  font-size: 25pt;
+  z-index: 100;
+}
+h4{
+  color: black;
+  font-family: starfont;
+  text-align: center;
+  font-size: 18pt;
+  margin: 10px;
+  text-decoration: overline underline;
+  display: block;
+  z-index: 10;
+}
+
+div.element{
+ margin: 0px;
+ padding: 0px;
+ margin-top: 15px;
+ width: 18%;
+ min-width: 210px;
+ height: 200px;
+ float: left;
+ display: block;
+}
+div.element_text{
+  float: left;
+  width: 80%;
+  display: block;
+}
+p{
+ color: black;
+ font-size:18pt;
+ text-align: left;
+}
+div.bio{
+  padding: 0px;
+  margin: 0px;
+  min-height: 200px;
+  width: 100%;
+  display: flex;
+}
+div.stats{
+  background-color: #e6e6e6;
+  border-radius: 25px;
+  box-shadow: 5px 10px #333;
+  padding: 8px;
+  margin: 10px;
+  width: 97%;
+  display: flex;
+}
+div.news{
+ margin-top: 70px;
+ display: block;
+ width 100%;
+ height: auto;
+
+}
+div.basics{
+  margin: 0px;
+  padding:0px;
+  width: 19%;
+  float: left;
+  padding-right: 10px;
+  border-right: 5px inset;
+  border-color: #333;
+}
+div.matchups{
+  margin: 0px;
+  padding: 0px;
+  width: 79%;
+  float: right;
+}
+
+table.basics{
+	margin: 0px;
+  width: 100%;
+}
+table.basics tr{
+}
+table.basics th{
+  font-family: arial;
+  text-decoration: underline;
+  font-size: 15pt;
+  color: #333;
+}
+table.basics td{
+  padding-bottom: 15px;
+	text-align: center;
+  font-size: 14pt;
+	color: black;
+}
+
+table.matchups{
+	margin-top: 10px;
+  width: 100%;
+  height: auto;
+}
+table.matchups tr{
+	font-size: 15pt;
+	color: #977700;
+}
+table.matchups th{
+  font-family: arial;
+  font-size: 15pt;
+  text-decoration: underline;
+  color: #333;
+}
+table.matchups td{
+  padding-bottom: 15px;
+	text-align: center;
+  font-size: 14pt;
+	color: black;
+}
+img.team_logo{
+ float: right;
+ height: 200px;
+ width: 200px;
+ margin-top: 0px;
+ margin-left: -200px;
+ margin-right: 1px;
+ //opacity: 0.4;
+ //z-index: 1000;
+}
+
+img.player{
+ height: 200px;
+ width: 200px;
+ margin: 20px;
+ margin-top: 0px;
+ margin-left: 1px;
+ float: left;
+ display: flex;
+}
+img.rSymbol{
+  width: 18px;
+  height: 18px;
+  margin:0px;
+  padding:0px;
+}
+
+div.gallery {
+    width: 280px;
+    height: 210px;
+}
+
+div.gallery img {
+    width: 210px;
+}
+
+</style>
